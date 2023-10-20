4462f4dff77fa45fec509700e7c4b6aed6fffdd3
dcfrq <dcfrqb@gmail.com>
2023-10-20 07:45:29 +0300

    Update taylor_series.cpp

--- taylor_series.cpp
+++ taylor_series.cpp
@@ -6,7 +6,7 @@
 #include <limits>
 
 //Подключение нужных библиотек
-using namespace std; //Подключение std
+using namespace std;
 
 //Функция для вывода красной строки без выхода из программы
 void outputRedStr(const string& str) {
